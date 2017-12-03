437bd3a988966010b422538aec3d36413d881aa6
Hunter Caron <huntercaron@mac.com>
2017-12-02 21:12:00 -0500

    fixing example file

--- eg/color-TCS34725.js
+++ eg/color-TCS34725.js
@@ -2,7 +2,7 @@ var five = require("../lib/johnny-five.js");
 var board = new five.Board();
 
 board.on("ready", function() {
-  var rgb = new five.Led.RGB([9, 10, 11]);
+  
   var color = new five.Color({
     controller: "TCS34725"
   });
@@ -10,4 +10,5 @@ board.on("ready", function() {
   color.on("change", function() {
     console.log("Color: ", five.Color.hexCode(this.rgb));
   });
+
 });
